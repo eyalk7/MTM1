85408d1555cfe9c606a2eba3a0665346b56de4ad
Eyal <eyal.kushnir@campus.technion.ac.il>
2019-04-08 14:30:56 +0300

    c

---
+++ eurovision.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "map.h"
+#include "eurovision.h"
+
+typedef struct JudgeData_t {
+    char *name;
+    unsigned int results[10];
+} *JudgeData;
+
+typedef struct StateData_t {
+    char *name;
+    char *song_name;
+    Map votes;
+} *StateData;
+
+struct eurovision_t {
+
+};
+
+main () {
+
+}
+
+Eurovision eurovisionCreate() {
+    Map States;
+    Map Judges;
+}
+
+void eurovisionDestroy(Eurovision eurovision) {
+
+}
+
+EurovisionResult eurovisionAddState(Eurovision eurovision, int stateId,
+                                    const char *stateName,
+                                    const char *songName) {
+
+}
+
+EurovisionResult eurovisionRemoveState(Eurovision eurovision, int stateId) {
+
+}
+
+EurovisionResult eurovisionAddJudge(Eurovision eurovision, int judgeId,
+                                    const char *judgeName,
+                                    int *judgeResults) {
+
+}
+
+EurovisionResult eurovisionRemoveJudge(Eurovision eurovision, int judgeId) {
+
+}
+
+EurovisionResult eurovisionAddVote(Eurovision eurovision, int stateGiver,
+                                   int stateTaker) {
+
+}
+
+EurovisionResult eurovisionRemoveVote(Eurovision eurovision, int stateGiver,
+                                      int stateTaker) {
+
+}
+
+List eurovisionRunContest(Eurovision eurovision, int audiencePercent) {
+
+}
+
+List eurovisionRunAudienceFavorite(Eurovision eurovision) {
+
+}
+
+List eurovisionRunGetFriendlyStates(Eurovision eurovision) {
+
+}
