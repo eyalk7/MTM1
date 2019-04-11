2fb79b9d9cbe5b89f64be47e73c4cd74b277c34e
daniel.r <daniel.r@campus.technion.ac.il>
2019-04-11 14:10:55 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -10,7 +10,7 @@ bool isIDValid(Map map, int id);
 bool isLowerCase(char c);
 bool checkValidName(char* name);
 
-int* createIntegerCopy(int num);
+int* copyInteger(int num);
 int compareIntegers(int a, int b);
 
 #endif //HW1_MTM_FUNCTIONS_H
