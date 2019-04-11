c60d26619909155682be70c927b7c709425ee755
daniel.r <daniel.r@campus.technion.ac.il>
2019-04-11 13:31:14 +0300

    Update state.c

--- state.c
+++ state.c
@@ -2,54 +2,55 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "map.h"
+#include "functions.h"
 
-typedef struct StateData_t {
+struct StateData_t {
     char *name;
     char *song_name;
     Map votes; // key = stateId, data = no. of votes this state gives
-} *StateData;
+};
 
 // copyStateDataElement - Function pointer to be used for copying data elements into	the map or when copying the map.
-    //memory allocation for StateDate element and check
-    //memory allocation for strings and check
-    //mapCopy for tmp_votes maps and check
-    //initialize the element
-    //return address
+//memory allocation for StateDate element and check
+//memory allocation for strings and check
+//mapCopy for tmp_votes maps and check
+//initialize the element
+//return address
 // copyStateKeyElement - Function pointer to be used for copying key elements into	the map or when copying the map.
-    //outside function
-        //allocation for int
-        //intialize the int
-        //return address
+//outside function
+//allocation for int
+//intialize the int
+//return address
 // freeStateDataElement - Function pointer to be used for removing data elements from the map
-    //mapDestroy the Votes map
-    //free strings alocations
-    //free stateData alocation
+//mapDestroy the Votes map
+//free strings alocations
+//free stateData alocation
 // freeStateKeyElement - Function pointer to be used for removing key elements from the map
-    //free allocation
+//free allocation
 // compareStateKeyElements - Function pointer to be used for comparing key elements inside the map. Used to check if new elements already exist in the map.
-    //outside function
-        //compare the ints
-        //return 0 if equal
-        //return bigger than 0 if first is bigger
-        //else the small than 0
+//outside function
+//compare the ints
+//return 0 if equal
+//return bigger than 0 if first is bigger
+//else the small than 0
 
 // copyVotesDataElement - Function pointer to be used for copying data elements into	the map or when copying the map.
-    //outside function
-        //allocation for int
-        //intialize the int
-        //return address
+//outside function
+//allocation for int
+//intialize the int
+//return address
 // copyVotesKeyElement - Function pointer to be used for copying key elements into	the map or when copying the map.
-    //outside function
-        //allocation for int
-        //intialize the int
-        //return address
+//outside function
+//allocation for int
+//intialize the int
+//return address
 // freeVotesDataElement - Function pointer to be used for removing data elements from the map
-    //free
+//free
 // freeVotesKeyElement - Function pointer to be used for removing key elements from the map
-    //free
+//free
 // compareVotesKeyElements - Function pointer to be used for comparing key elements inside the map. Used to check if new elements already exist in the map.
-    //outside function
-        //compare the ints
-        //return 0 if equal
-        //return bigger than 0 if first is bigger
-        //else the small than 0
+//outside function
+//compare the ints
+//return 0 if equal
+//return bigger than 0 if first is bigger
+//else the small than 0
