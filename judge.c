d80cf4c11f48e47b1cd015b4e04f0b5cc35f3389
daniel.r <daniel.r@campus.technion.ac.il>
2019-04-11 14:11:43 +0300

    Update judge.c

--- judge.c
+++ judge.c
@@ -9,7 +9,7 @@
 
 struct JudgeData_t {
     char *name;
-    unsigned int states[NUMBER_OF_STATES_TO_RANK];   // list of stateIds in order of ranking
+    unsigned int states[NUMBER_OF_STATES_TO_RANK];   // list of stateIds in order of points to give
 };
 
 
@@ -26,28 +26,28 @@ JudgeDataElement copyJudgeDataElement(JudgeDataElement data) {
     }
     //copy name and results
     strcpy(copy->name, ((JudgeData)data)->name);
-    
+
     for (int i = 0; i < NUMBER_OF_STATES_TO_RANK; i++) {
         copy->states[i] = ((JudgeData)data)->states[i];
     }
-    
+
     //return address
     return copy;
 }
 
 // copyJudgeKeyElement - Function pointer to be used for copying key elements into	the map or when copying the map.
-int* copyJudgeKeyElement(int* key) {
-    return copyInteger(*key);
+JudgeKeyElement copyJudgeKeyElement(JudgeKeyElement key) {
+    return copyInteger(*((int*)key));
 }
 
 // freeJudgeDataElement - Function pointer to be used for removing data elements from the map
-void freeJudgeDataElement(JudgeData data) {
+void freeJudgeDataElement(JudgeDataElement data) {
     //free strings allocation
     //free judgeData allocation
 }
 
 // freeJudgeKeyElement - Function pointer to be used for removing key elements from the map
-void freeJudgeKeyElement(int* key) {
+void freeJudgeKeyElement(JudgeKeyElement key) {
     //free allocation
 }
 
