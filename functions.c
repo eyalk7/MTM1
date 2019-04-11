75c735a69b555ce61d9e8eba24f793c78fc5040b
daniel.r <daniel.r@campus.technion.ac.il>
2019-04-11 13:30:05 +0300

    outside/helper functions file

---
+++ functions.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <stdlib.h>
+#include "functions.h"
+
+bool isIDValid(Map map, int id) {
+    //check ID >= 0
+    //check with mapContain if Id already exist
+    return (id >= 0) && !mapContains(map, &id);
+}
+
+bool isLowerCase(char c) {
+    return ('a' <= c && c <= 'z');
+}
+
+bool checkValidName(char* name) {
+    //check the given string only contains small letters and spaces
+    for (int i = 0; i < strlen(name); i++) {
+        if (!isLowerCase(name[i]) && name[i] != SPACE) return false;
+    }
+    return true;
+}
+
+int* createInteger(int num) {
+    //allocation for int
+    int* copy = malloc(sizeof(*copy));
+    //initialize the int
+    *copy = num;
+    //return address
+    return copy;
+}
+
+int compareIntegers(int a, int b) {
+    //compare the ints
+    //return 0 if equal
+    //return bigger than 0 if first is bigger
+    //else the small than 0
+}
