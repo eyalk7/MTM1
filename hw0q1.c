43093b9c02e543cf300eeab6166157ffaab9f91f
daniel.r <daniel.r@campus.technion.ac.il>
2019-04-12 12:45:56 +0300

    Update hw0q1.c

--- hw0q1.c
+++ hw0q1.c
@@ -2,19 +2,27 @@
 #include <string.h>
 #include <assert.h>
 
-char *stringDuplicator(char *s, int times) {
-    // (1)bug changed to assert without !
-    assert(s);
+#include <stdio.h>
+
+char* stringDuplicator(char* str, int times);
+
+int main() {
+    char* out = stringDuplicator("Hello", 3);
+    if (out)
+        printf("%s", out);
+
+    return 0;
+}
+
+char *stringDuplicator(char *str, int times) {
+    assert(str);
     assert(times > 0);
-    // (1)convension LEN -> len
-    int len = strlen(s);
-    // (2)bug +1 in the malloc for end of string
-    char *out = malloc(len*times+1);
-    // (2)convension assert not for internal function problems like in NULL malloc
-    if (out == 0) return NULL;
+    int len = strlen(str);
+    char *out = malloc(len * times + 1);
+    if (out == NULL) return NULL;
     for (int i = 0; i < times; i++) {
-    // (3)bug not to change the out pointer, instead use interator
-        strcpy(out+i*len, s);
+        strcpy(out + (i * len), str);
     }
     return out;
 }
+
