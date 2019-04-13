95143dcf5985b70cb4a08b24cb3df64cf05f9028
daniel.r <daniel.r@campus.technion.ac.il>
2019-04-13 12:10:11 +0300

    Update judge.h

--- judge.h
+++ judge.h
@@ -1,12 +1,11 @@
 #ifndef JUDGES_H
 #define JUDGES_H
 
+#include <stdbool.h>
+
 #define NUMBER_OF_STATES_TO_RANK 10
 
-typedef struct JudgeData_t {
-    char *name;
-    unsigned int states[NUMBER_OF_STATES_TO_RANK];   // list of stateIds in order of points to give
-} *JudgeData;
+typedef struct JudgeData_t *JudgeData;
 
 typedef void* JudgeDataElement;
 typedef void* JudgeKeyElement;
@@ -16,5 +15,7 @@ JudgeKeyElement copyJudgeKeyElement(JudgeKeyElement key);
 void freeJudgeDataElement(JudgeDataElement data);
 void freeJudgeKeyElement(JudgeKeyElement key);
 int compareJudgeKeyElements(JudgeKeyElement key1, JudgeKeyElement key2);
+
+bool resultsContainState(JudgeDataElement judge, int stateId);
 
 #endif //JUDGES_H
