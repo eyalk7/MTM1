c9be33e962216b4ff867f95ca0f9a67cfa76a170
daniel.r <daniel.r@campus.technion.ac.il>
2019-04-14 21:13:46 +0300

    Update state.h

--- state.h
+++ state.h
@@ -7,17 +7,32 @@ typedef struct StateData_t {
     Map votes; // key = stateId, data = no. of votes this state gives
 } *StateData;
 
+typedef struct countTable_t {
+    unsigned int id;
+    int count;
+}* CountTable;
+
 typedef void* StateDataElement;
 typedef void* StateKeyElement;
 
 typedef void* VoteDataElement;
 typedef void* VoteKeyElement;
 
+/********************** STATE MAP FUNCTIONS ***********************/
+
 StateDataElement copyStateDataElement(StateDataElement data);
 StateKeyElement copyStateKeyElement(StateKeyElement key);
 void freeStateDataElement(StateDataElement data);
 void freeStateKeyElement(StateKeyElement key);
 int compareStateKeyElements(StateKeyElement key1, StateKeyElement key2);
+
+/********************** COUNT TABLE STRUCT FUNCTIONS ***********************/
+
+CountTable* countTableCreate(Map map);
+CountTable* convertVotesToArray(Map votes);
+
+
+/********************** VOTE MAP FUNCTIONS ***********************/
 
 VoteDataElement copyVoteDataElement(VoteDataElement data);
 VoteKeyElement copyVoteKeyElement(VoteKeyElement key);
