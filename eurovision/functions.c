1089b0b0d615e333cb39beb35cd62c53857f8732
Eyal <eyal.kushnir@campus.technion.ac.il>
2019-05-01 13:27:22 +0300

    c

--- eurovision/functions.c
+++ eurovision/functions.c
@@ -320,11 +320,11 @@ void calculateFinalPoints(List audience_points, List judge_points,
 
     }
 
-    listSort(audience_points, compareStatePoints);
-
-    int place = 0;
-    LIST_FOREACH(StatePoints, points, audience_points) {
-        printf("%d) state: %d - points: %f\n", place, points->id, points->points);
-        place++;
-    }
+//    listSort(audience_points, compareStatePoints);
+//
+//    int place = 0;
+//    LIST_FOREACH(StatePoints, points, audience_points) {
+//        printf("%d) state: %d - points: %f\n", place, points->id, points->points);
+//        place++;
+//    }
 }
